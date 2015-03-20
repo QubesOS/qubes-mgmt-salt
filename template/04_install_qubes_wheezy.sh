ab83d02c9b9c217baff7b51ca5c1223282d86614
Jason Mehring <nrgaway@gmail.com>
2015-03-20 04:09:45 -0400

    Added some backports wheezy needs to install salt

--- template/04_install_qubes_wheezy.sh
+++ template/04_install_qubes_wheezy.sh
@@ -1,1 +1,29 @@
-04_install_qubes_jessie.sh
+#!/bin/bash -e
+# vim: set ts=4 sw=4 sts=4 et :
+
+source "${SCRIPTSDIR}/vars.sh"
+source "${SCRIPTSDIR}/distribution.sh"
+
+#### '----------------------------------------------------------------------
+info ' Trap ERR and EXIT signals and cleanup (umount)'
+#### '----------------------------------------------------------------------
+trap cleanup ERR
+trap cleanup EXIT
+
+#### '----------------------------------------------------------------------
+info ' Installing init-system-helpers'
+#### '----------------------------------------------------------------------
+DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true \
+    chroot apt-get ${APT_GET_OPTIONS} -t wheezy-backports install python-requests python-zmq
+
+#### '-------------------------------------------------------------------------
+info ' Installing Qubes Salt Management Flavor'
+#### '-------------------------------------------------------------------------
+aptInstall salt-common salt-minion qubes-salt-config
+
+#### '----------------------------------------------------------------------
+info ' Cleanup'
+#### '----------------------------------------------------------------------
+trap - ERR EXIT
+trap
+
