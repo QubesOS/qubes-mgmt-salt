f88870867ba634d2b6bdd90d7c1aa5911baa5242
Jason Mehring <nrgaway@gmail.com>
2015-05-20 04:57:11 -0400

    Re-worked grain logic to since it was not compatible with salt-2015.02

--- srv/qubes-salt/dom0/services/_grains/qubes_dom0.py
+++ srv/qubes-salt/dom0/services/_grains/qubes_dom0.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
+
+import salt.config
+import salt.grains.core
+
+__opts__ = salt.config.minion_config('/etc/salt/minion')
+salt.grains.core.__opts__ = __opts__
+
+
 def qubes_dom0():
     '''
     Redefine qubes grains.
     '''
     grains = {}
-    if __salt__['core.os_data']()['os'] == 'Qubes':
+    if salt.grains.core.os_data()['os'] == 'Qubes': 
         grains = {}
         grains['virtual'] = 'Qubes'
         grains['os'] = 'Fedora'
