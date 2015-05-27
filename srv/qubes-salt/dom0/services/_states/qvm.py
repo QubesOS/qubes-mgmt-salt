c569b709454e0b392f12bcb0add4d2191835466f
Jason Mehring <nrgaway@gmail.com>
2015-05-27 07:45:01 -0400

    Cleanup and converted all state calls to use new API
    - Renamed `dead` to `halted`

--- srv/qubes-salt/dom0/services/_states/qvm.py
+++ srv/qubes-salt/dom0/services/_states/qvm.py
@@ -21,22 +21,15 @@ Management declarations are typically rather simple:
 
 # Import python libs
 import logging
-#import os
-#import re
 import collections
 
 # Salt libs
-import salt.utils
 from salt.output import nested
-#from salt.utils import namespaced_function as _namespaced_function
-from salt.utils.odict import OrderedDict as _OrderedDict
-#from salt._compat import string_types
 from salt.exceptions import (
-    CommandExecutionError, MinionError, SaltInvocationError
+    CommandExecutionError, SaltInvocationError
 )
 
 # Other salt related utilities
-from qubes_utils import tolist as _tolist
 from qubes_utils import update as _update
 
 log = logging.getLogger(__name__)
@@ -52,9 +45,6 @@ def __virtual__():
 '''
 TODO:
 =====
-
-- Consider creating an additional state that will allow all vm related
-  configurations to be within one state (qvm.vm)
 
 - Functions to implement (qvm-commands):
   [ ] Not Implemented
@@ -83,130 +73,62 @@ def _nested_output(obj):
     return ret
 
 
-# XXX: rename to something more descriptive; used to get error code and output only
-def _call_function(name, function, *varargs, **kwargs):
+def _vm_action(name, _action, *varargs, **kwargs):
+    '''
+    Start, shutdown, kill, pause, unpause, etc
+    '''
+
+    # ==========================================================================
+    # DEBUG ONLY
+    # XXX: TODO: Move globally, so it can be set via state file globally
+    debug         = False
+    debug_changes = False
+    strict        = False
+
+    if strict:
+        kwargs.setdefault('flags', [])
+        kwargs['flags'].append('strict')
+    if debug:
+        kwargs.setdefault('result-mode', [])
+        kwargs['result-mode'].append('debug')
+        kwargs.setdefault('result-mode', [])
+    if debug_changes:
+        kwargs['result-mode'].append('debug-changes')
+    # ==========================================================================
+
+    stdout = stderr = ''
     ret = {'name': name,
+           'retcode': 0,
+           'result': False,
+           'comment': '',
+           'changes': {},
            'stdout': '',
            'stderr': '',
-           'retcode': 0,
-           'changes': {},
           }
 
-    result = __salt__[function](name, *varargs, **kwargs)
-    ret.update(result)
+    try:
+        result = __salt__[_action](name, *varargs, **kwargs)
+        stderr += result['stderr']
+        stdout += result['stdout']
+        _update(ret, result, create=True)
+    except (SaltInvocationError, CommandExecutionError), e:
+        ret['retcode'] = 1
+        stderr += e.message + '\n'
 
-    ret['comment'] = ret['stdout']
+    if not ret['comment']:
+        if stderr:
+            #ret['result'] = False
+            if stdout:
+                stdout = '====== stdout ======\n{0}\n\n'.format(stdout)
+            stderr = '====== stderr ======\n{0}'.format(stderr)
+        ret['comment'] = stdout + stderr
 
     if __opts__['test']:
         ret['result'] = None if not ret['retcode'] else False
     else:
         ret['result'] = True if not ret['retcode'] else False
+
     return ret
-
-
-#def clone(name, clone_name, *varargs, **kwargs):
-def OLDclone(name, *varargs, **kwargs):
-    '''
-    '''
-    ret = {'name': name,
-           'changes': {},
-           'result': False,
-           'comment': ''}
-
-    args, fnargs = salt.utils.arg_lookup(__salt__['qvm.clone']).values()
-    for key, value in kwargs.items():
-        if key in fnargs:
-            fnargs[key] = value
-
-    # Support test mode only
-    if __opts__['test'] == True:
-        # Pre-check if create should succeed
-        ret = _call_function(name, 'qvm.check')
-        ret['result'] = not ret['result']
-        if not ret['result']:
-            return ret
-        ret['result'] = None
-        ret['comment'] = 'VM {0} will be cloned\n{1}'.format(name, _nested_output(fnargs))
-        return ret
-
-    #fnargs['clone_name'] = clone_name
-    ret = _call_function(name, 'qvm.clone', *varargs, **fnargs)
-    return ret
-
-
-def service(name, *varargs, **kwargs):
-    '''
-    Manage vmname service (qvm-service)
-    '''
-    ret = {'name': name,
-           'changes': {},
-           'result': False,
-           'comment': ''}
-
-    stdout = stderr = ''
-    services = []
-
-    # Send as *varargs...
-    # ===================
-    for key, value in kwargs.items():
-        try:
-            qvm_service = [key]
-            qvm_service.extend(_tolist(value))
-            result = _call_function(name, 'qvm.service', *qvm_service, **kwargs)
-            # XXX: Don't send kwargs since varargs are being used instead -- handled in module now
-            #result = _call_function(name, 'qvm.service', *qvm_service)
-        #except CommandExecutionError, e:
-        except (SaltInvocationError, CommandExecutionError), e:
-            stderr += e.message + '\n'
-            continue
-        stderr += result['stderr']
-        stdout += result['stdout']
-        _update(ret, result, create=True, append=['cmd', 'stderr', 'stdout'])
-
-    if stderr:
-        ret['result'] = False
-        stderr = 'stderr:\n{0}'.format(stderr)
-        if stdout:
-            stderr += 'stdout:\n'
-    ret['comment'] = stderr + stdout
-    return ret
-
-
-def _vm_action(name, _action, *varargs, **kwargs):
-    '''
-    Start, shutdown, kill, pause, unpause, etc
-    '''
-    # ==========================================================================
-    # XXX: DEBUG ONLY
-    strict = False
-    if strict:
-        kwargs.setdefault('flags', [])
-        kwargs['flags'].append('strict')
-    # ==========================================================================
-
-    stdout = stderr = ''
-    ret = {'name': name,
-           'changes': {},
-           'result': False,
-           'comment': ''}
-
-    try:
-        result = _call_function(name, _action, *varargs, **kwargs)
-        stderr += result['stderr']
-        stdout += result['stdout']
-        _update(ret, result, create=True)
-    except (SaltInvocationError, CommandExecutionError), e:
-        stderr += e.message + '\n'
-
-    if stderr:
-        ret['result'] = False
-        if stdout:
-            stdout = '====== stdout ======\n{0}\n\n'.format(stdout)
-        stderr = '====== stderr ======\n{0}'.format(stderr)
-    ret['comment'] = stdout + stderr
-    return ret
-
-# ==============================================================================
 
 
 def exists(name, *varargs, **kwargs):
@@ -236,36 +158,13 @@ def running(name, *varargs, **kwargs):
     return _vm_action(name, 'qvm.state', *varargs, **kwargs)
 
 
-def dead(name, *varargs, **kwargs):
+def halted(name, *varargs, **kwargs):
     '''
     Returns True is vmname is halted
     '''
     varargs = list(varargs)
-    varargs.append('dead')
+    varargs.append('halted')
     return _vm_action(name, 'qvm.state', *varargs, **kwargs)
-
-
-def create(name, *varargs, **kwargs):
-    '''
-    Create vmname (qvm-create)
-    '''
-    #kwargs.setdefault('flags', [])
-    #kwargs['flags'].append('quiet')
-    return _vm_action(name, 'qvm.create', *varargs, **kwargs)
-
-
-def remove(name, *varargs, **kwargs):
-    '''
-    Remove vmname (qvm-remove)
-    '''
-    return _vm_action(name, 'qvm.remove', *varargs, **kwargs)
-
-
-def clone(name, target, *varargs, **kwargs):
-    '''
-    Clone a VM (qvm-clone)
-    '''
-    return _vm_action(name, 'qvm.clone', target, *varargs, **kwargs)
 
 
 def start(name, *varargs, **kwargs):
@@ -307,11 +206,25 @@ def unpause(name, *varargs, **kwargs):
     return _vm_action(name, 'qvm.unpause', *varargs, **kwargs)
 
 
-def prefs(name, *varargs, **kwargs):
+def create(name, *varargs, **kwargs):
     '''
-    Sets vmname preferences (qvm-prefs)
+    Create vmname (qvm-create)
     '''
-    return _vm_action(name, 'qvm.prefs', *varargs, **kwargs)
+    return _vm_action(name, 'qvm.create', *varargs, **kwargs)
+
+
+def remove(name, *varargs, **kwargs):
+    '''
+    Remove vmname (qvm-remove)
+    '''
+    return _vm_action(name, 'qvm.remove', *varargs, **kwargs)
+
+
+def clone(name, target, *varargs, **kwargs):
+    '''
+    Clone a VM (qvm-clone)
+    '''
+    return _vm_action(name, 'qvm.clone', target, *varargs, **kwargs)
 
 
 def run(name, *varargs, **kwargs):
@@ -321,21 +234,34 @@ def run(name, *varargs, **kwargs):
     return _vm_action(name, 'qvm.run', *varargs, **kwargs)
 
 
-# XXX: Convert to _vm_action ???
+def prefs(name, *varargs, **kwargs):
+    '''
+    Sets vmname preferences (qvm-prefs)
+    '''
+    return _vm_action(name, 'qvm.prefs', *varargs, **kwargs)
+
+
+def service(name, *varargs, **kwargs):
+    '''
+    Manage vmname service (qvm-service)
+    '''
+    return _vm_action(name, 'qvm.service', *varargs, **kwargs)
+
+
 def vm(name, *varargs, **kwargs):
     '''
     Wrapper to contain all VM state functions
 
     create, remove, clone
     prefs, service
-    exists, running, absent, dead
+    exists, running, absent, halted
     start, stop, pause, unpause
     '''
     actions = [
         'exists',
         'running',
         'absent',
-        'dead',
+        'halted',
         'remove',
         'create',
         'clone',
@@ -362,9 +288,6 @@ def vm(name, *varargs, **kwargs):
 
     # Global Strict mode from state file
     strict = kwargs.pop('strict', False)
-
-    #def to_dict(alist):
-    #    return reduce(lambda r, d: r.update(d) or r, alist, {})
 
     def parse_options(options):
         varargs = []
@@ -397,37 +320,14 @@ def vm(name, *varargs, **kwargs):
                 #ret['changes'].update(result['changes'])
                 ret['changes']['qvm.' + action] = result['changes']
 
-            if 'result' in result and not result['result'] and result['result'] is not None:
-                # Record failure
-                ret['result'] = False
-
-                try:
-                    stderr += '=== \'{0}\' {1} ===\n'.format(action, result['retcode'])
-                except KeyError:
-                    stderr += '=== \'{0}\' {1} ===\n'.format(action, result['result'])
-
-                # XXX: This may not enable TEST mode
-                # Switch to test mode for remainder of action items since we do
-                # not want them to execute but may find the results useful
-                __opts__['test'] = True
-
-                # XXX: Consider breaking out of loop on error...
-                #      CREATE a YAML option to allow user to decide
-
-            message = None
-            if 'stderr' in result and result['stderr'].strip():
-                stderr += '====== stderr \'{0}\' ======\n'.format(action)
-                stderr += result['stderr'] + '\n'
+            if 'comment' in result and result['comment'].strip():
+                stdout += '====== [\'{0}\'] ======\n'.format(action)
+                stdout += result['comment'] + '\n'
                 message = True
 
-            if 'stdout' in result and result['stdout'].strip():
-                stdout += '====== stdout \'{0}\' ======\n'.format(action)
-                stdout += result['stdout'] + '\n'
-                message = True
-
-            if not message:
-                if 'cmd' in result and result['cmd']:
-                    stdout += '====== \'{0}\' {1} ======\n{2}\n'.format(action, result['result'], result['cmd'])
+            #if not message:
+            #    if 'cmd' in result and result['cmd']:
+            #        stdout += '====== \'{0}\' {1} ======\n{2}\n'.format(action, result['result'], result['cmd'])
 
             if stdout and index < len(actions):
                 stdout += '\n'
@@ -435,9 +335,6 @@ def vm(name, *varargs, **kwargs):
     # Reset test back to original selected mode if it was modified
     if test != __opts__['test']:
         __opts__['test'] = test
-
-    #if not __opts__['test'] and ret['result'] is None:
-    #    ret['result'] = False
 
     # If result is still None; and no tests are running, nothing was run
     if __opts__['test'] == True:
