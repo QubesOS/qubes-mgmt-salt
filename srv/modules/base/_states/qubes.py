ce053c61d4e49b7c30046e0da3f8b04d659d22c3
Jason Mehring <nrgaway@gmail.com>
2015-06-19 04:36:14 -0400

    Addedd a generic qubes state to hold misc states. Created an status state to be used when not running a state at all

---
+++ srv/modules/base/_states/qubes.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+'''
+:maintainer:    Jason Mehring <nrgaway@gmail.com>
+:maturity:      new
+:platform:      all
+
+==========================
+Qubes misc state functions
+==========================
+'''
+
+# Import python libs
+import logging
+
+# Salt libs
+from salt.exceptions import (
+    CommandExecutionError, SaltInvocationError
+)
+
+# Salt + Qubes libs
+from qubes_utils import Status
+
+log = logging.getLogger(__name__)
+
+
+def __virtual__():
+    return 'qubes'
+
+
+def _state_action(_action, *varargs, **kwargs):
+    '''
+    '''
+    try:
+        status = __salt__[_action](*varargs, **kwargs)
+    except (SaltInvocationError, CommandExecutionError), e:
+        status = Status(retcode=1, result=False, comment=e.message + '\n')
+    return dict(status)
+
+
+def status(name, comment, result=None):
+    '''
+    Used to show an alert message when a condition is met not to include a state.
+    '''
+    return dict(Status(name=name, result=result, comment=comment))
