QubesOS/qubes-mgmt-salt

commit 2b9a084721d1ad3ffb6ef80e3d8524a295d391ee

Build qubes-mgmt-admin-tools package only for dom0
This tool isn't useful in a VM (at least for now). Skip its build in a
VM, as it's a python2 code (and needs to be because of fc25 based dom0
uses python2 salt build).

QubesOS/qubes-issues#5761

Makefile
@@ -30,10 +30,6 @@ build:
 
 .PHONY: install-custom
 install-custom:: 
-	# Install qubesctl
-	@install -d -m 0755 $(DESTDIR)$(BINDIR)
-	@install -p -m 0755 qubesctl $(DESTDIR)$(BINDIR)
-	
 	# Install /etc/salt/* and /srv/*
 	cp -Tr etc $(DESTDIR)/etc
 	cp -Tr srv $(DESTDIR)/srv
@@ -51,6 +47,9 @@ install-vm:
 install-dom0:
 	python2 setup.py install -O1 --root $(DESTDIR)
 	make install -C doc
+	# Install qubesctl
+	install -d -m 0755 $(DESTDIR)$(BINDIR)
+	install -p -m 0755 qubesctl $(DESTDIR)$(BINDIR)
 
 .PHONY: get-sources
 get-sources: GIT_REPOS := $(addprefix $(SRC_DIR)/,$(MGMT_SALT_COMPONENTS) mgmt-salt-app-saltstack)

rpm_spec/qubes-mgmt-salt.spec.in
@@ -1,3 +1,10 @@
+# build admin-tools package only on dom0 version (it's python2)
+%if 0%{?fedora} == 25
+%global with_admintools 1
+%else
+%global with_admintools 0
+%endif
+
 Name:      qubes-mgmt-salt
 Version:   @VERSION@
 Release:   1%{?dist}
@@ -12,8 +19,10 @@ Requires:  salt-minion
 Requires:  qubes-mgmt-salt-base
 BuildRequires: tree
 BuildRequires: pandoc
+%if %{with_admintools}
 BuildRequires: python-setuptools
 BuildRequires: python2-devel
+%endif
 
 Source0: %{name}-%{version}.tar.gz
 
@@ -57,6 +66,7 @@ Requires:  qubes-mgmt-salt
 %description shared-formulas
 Qubes+Salt Management shared (qubes-mgmt-all-*) formulas.
 
+%if %{with_admintools}
 %package admin-tools
 Summary:    Management tools integrating dom0 and VM management
 Group:     System administration tools
@@ -65,19 +75,21 @@ Requires:  qubes-mgmt-salt
 
 %description admin-tools
 Tools to integrate dom0 and VM management into a single qubesctl tool.
+%endif
 
 %prep
 %setup -q
 
 %install
 make install DESTDIR=%{buildroot} LIBDIR=%{_libdir} BINDIR=%{_bindir} SBINDIR=%{_sbindir} SYSCONFDIR=%{_sysconfdir}
-make install-dom0 DESTDIR=%{buildroot}
+%if %{with_admintools}
+make install-dom0 DESTDIR=%{buildroot} BINDIR=%{_bindir} SBINDIR=%{_sbindir}
+%endif
 rm -rf %{buildroot}/srv/formulas/.gitignore
 
 %files
 %defattr(-,root,root)
 %attr(766, root, root)
-%{_mandir}/man1/qubesctl.1*
 
 %files config
 %defattr(-,root,root)
@@ -103,13 +115,15 @@ rm -rf %{buildroot}/srv/formulas/.gitignore
 /srv/salt/top.jinja
 /srv/salt/top.sls.old
 
+%if %{with_admintools}
 %files admin-tools
 /usr/bin/qubesctl
+%{_mandir}/man1/qubesctl.1*
 %dir %{python2_sitelib}/qubessalt-*egg-info
 %{python2_sitelib}/qubessalt-*egg-info/*
 %dir %{python2_sitelib}/qubessalt
 %{python2_sitelib}/qubessalt/__init__.py*
-
+%endif
 
 %files shared-formulas
 %defattr(-,root,root)

commit 11b1638605f0d1794559f784e9ad93b6f7088591

Restore creating $(DESTDIR) in install-custom
It is needed by the cp call. In Debian build, no other target create
that directory.

Fixes: 2b9a084 "Build qubes-mgmt-admin-tools package only for dom0"

Makefile
@@ -30,6 +30,7 @@ build:
 
 .PHONY: install-custom
 install-custom:: 
+	install -d -m 0755 $(DESTDIR)
 	# Install /etc/salt/* and /srv/*
 	cp -Tr etc $(DESTDIR)/etc
 	cp -Tr srv $(DESTDIR)/srv